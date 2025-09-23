66d4c48888f54f5f5d3286387457259776769f31
Twisuki <Twisuki@outlook.com>
2025-09-23 21:59:31 +0800

    :construction: bg组件

--- app/components/base/background.vue
+++ app/components/base/background.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
+interface BackGround {
+  url?: string | null
+  css?: string
+}
 
+const props = withDefaults(defineProps<BackGround>(), {
+  url: "/shizuku.png",
+});
 </script>
 
 <template>
+  <div
+      class="bg"
+      :style="{
+      backgroundImage: props.css ? undefined : `url(${props.url})`,
+      background: props.css,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }"
+  />
 </template>
 
 <style scoped>
-
+.bg {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
